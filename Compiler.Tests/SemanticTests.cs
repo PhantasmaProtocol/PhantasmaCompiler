1db6b7ce515022ea0c668b0abbb7d57adc005d25
Sergio Flores <relfos@gmail.com>
2018-11-16 12:26:12 +0000

    Folder reorganization

---
+++ Compiler.Tests/SemanticTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.Linq;
+
+namespace Phantasma.Tests
+{
+    [TestClass]
+    public class SemanticTests
+    {
+        [TestMethod]
+        public void TestDecimals()
+        {
+//            Assert.IsTrue(n == TokenUtils.ToBigInteger(d, places));
+        }
+
+    }
+}
